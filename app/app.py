6763748747e57933441319cd3cadede113c48bc6
DESKTOP-7SAFABN\Morgan <morgandally4@gmail.com>
2018-05-03 15:13:55 +1200

    More navigation through pages. Trying to set up basis of the website to build onto.

--- app/app.py
+++ app/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af4b76779b9eb953950f40167b574122d41a1925bf0902aa44ccd4cd594915ae
-size 1951
+oid sha256:430d901d405a25c5bb37dfede0eb8ec2dd8b6a7523627caabc1397da487af46c
+size 2598
