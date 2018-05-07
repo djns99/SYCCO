e19f3aaad4d338496683b5333d183a3b9e7300a6
Daniel Stokes <daniel.stokes@nyriad.com>
2018-05-07 20:09:30 +1200

    Integrated image labelling page

--- app/app.py
+++ app/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:430d901d405a25c5bb37dfede0eb8ec2dd8b6a7523627caabc1397da487af46c
-size 2598
+oid sha256:b9349b82127645c154917b86a4925a11d8978aed7e5611a4a52e3314b19b7544
+size 2667
---
+++ app/static/scripts/label-image.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9517b0e8dcad96b39f22d3ebdc456fa1d51634e9220a2f868cdaa52a5a29615c
+size 2494
