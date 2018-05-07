a5dd478bca14b81f6ee06b5e6865e7424b0898a1
Daniel Stokes <daniel.stokes@nyriad.com>
2018-05-07 20:18:42 +1200

    Fixed resize bug

--- app/static/scripts/label-image.js
+++ app/static/scripts/label-image.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9517b0e8dcad96b39f22d3ebdc456fa1d51634e9220a2f868cdaa52a5a29615c
-size 2494
+oid sha256:d4319f503fe5b729f34ad5269379fe5c3acd5487eb7dd1e6131dfcbe1a91672d
+size 2581
