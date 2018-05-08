cfb2d7a8e66ae78e66cd2682d579999252b05bf2
Daniel Stokes <djns1@students.waikato.ac.nz>
2018-05-09 11:33:01 +1200

    Fixed image labelling canvas resize

--- app/static/scripts/label-image.js
+++ app/static/scripts/label-image.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4319f503fe5b729f34ad5269379fe5c3acd5487eb7dd1e6131dfcbe1a91672d
-size 2581
+oid sha256:88aa63532ecb92f12081106e433d4a86465f19f89b2fd1d1e986a460072665c2
+size 2885
