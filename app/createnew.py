9357eb0df5a5f84db551850743cc649a9c044f79
Daniel Stokes <daniel.stokes@nyriad.com>
2018-05-14 19:52:32 +1200

    Small cleanup

--- app/createnew.py
+++ app/createnew.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a4062615de0c393763c00622f0c47d3fa827306e0164b6e73eb2e21ce55673
-size 718
+oid sha256:d3f4d7cdeefc364ae87a721ad913767176eadfb1ebca2d6391b9bedf153b7e4d
+size 721
