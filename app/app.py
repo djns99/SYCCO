b53479b7e4be139fd1d394c34469a95f40413c35
Daniel Stokes <daniel.stokes@nyriad.com>
2018-05-14 21:17:15 +1200

    Prevent user from accessing upload etc. without selecting project

--- app/app.py
+++ app/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5dd47b7d0182639a3deadf8c6172e9a2836ce646b62795c826c103512326768
-size 4083
+oid sha256:a0a54543d0e4d7da019b119ff7f6b40f55e7756d85c9f11f56d529c58ad8d8ae
+size 4884
