8fc7a85016e712771d05d325af24c19c67efe2e1
Daniel Stokes <djns1@students.waikato.ac.nz>
2018-05-23 13:09:45 +1200

    Created prototype prediction page

--- app/app.py
+++ app/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:128ccdf02d9d260ab8f13f570cfdeb3177b252eb69babca6a5461368ebd1d19d
-size 7120
+oid sha256:f53b247e72fd0b0e84f28bebba09c98a65ce3f29d33cb3feb40ac604a853812d
+size 9157
---
+++ app/static/scripts/training-graph.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d0b5907d46371d93b38ba57234369f029fe3ee64b9ffb8f3edc826108e0f0bd
+size 952
